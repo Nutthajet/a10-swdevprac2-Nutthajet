3c589dd7315f2bee598b9f1c663dc93285e7e11d
Nutthajet <tin.nutthajet@gmail.com>
2026-03-22 23:48:37 +0700

    Final

--- src/components/VenueCatalog.tsx
+++ src/components/VenueCatalog.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import ProductCard from "./Card";
+import { VenueJson, VenueItem } from "../../interfaces";
 
 export default function VenueCatalog({venuesJson}:{venuesJson:VenueJson}){
     return (
